2863d7f21e062385dbb334b226b66a7bd873e82b
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-05 17:42:44 -0300

    Atualizar Readme

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿// Este programa realiza uma requisição à API CheapShark para obter uma lista de promoções de jogos disponíveis.
 // Ele utiliza o endpoint https://www.cheapshark.com/api/1.0/deals para buscar os dados e exibe as promoções 
 // na tela de forma organizada, incluindo informações como título do jogo, preço original, preço com desconto e percentual de economia.
-
-
 using (HttpClient client = new HttpClient())
 {
     string resposta = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
